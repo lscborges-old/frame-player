7e466fe5cf2ae61f6c034000f1c43d20eedab7dc
Lucas Borges <lscborges1@gmail.com>
2022-02-20 21:09:27 -0300

    removed log

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import './index.css'
 const frames = importAll(require.context('./assets/frames', false, /\.(png|jpe?g|svg)$/));
 
 const framesArray = Object.keys(frames);
-console.log(framesArray);
 
 function App() {
   return (
